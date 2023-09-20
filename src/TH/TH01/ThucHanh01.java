08fd7e1cc430c6175a5d29baa0c797cbf61acb07
tanran031199 <manhthangthang@gmail.com>
2023-09-20 14:42:46 +0700

    upload

---
+++ src/TH/TH01/ThucHanh01.java
@@ -0,0 +1,11 @@
+package TH.TH01;
+
+import java.util.Date;
+
+public class ThucHanh01 {
+    public static void main(String[] arrgs) {
+        System.out.println("Hello world!");
+        Date date = new Date();
+        System.out.println("Time now: " + date);
+    }
+}
